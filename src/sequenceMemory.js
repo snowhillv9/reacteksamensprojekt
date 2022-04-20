0471152903b1ff1cf492466ee162374de7b95eef
snowhillv9 <81157167+snowhillv9@users.noreply.github.com>
2022-04-20 13:33:39 +0200

    xd
    xd

--- src/sequenceMemory.js
+++ src/sequenceMemory.js
@@ -1,3 +1,9 @@
+import React, { useEffect, useState } from "react";
+
 let squareArray = [0,1,2,3,4,5,6,7,8];
 
 let memoryArray = [];
+
+let squareToAdd = Math.floor(Math.random() * 9);
+
+memoryArray.push(squareToAdd);
