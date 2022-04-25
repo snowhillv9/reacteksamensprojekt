65e81ac756ebc617eb98d6a6b3582aa0cac01b70
kre1e <olesendk@hotmail.com>
2022-04-25 12:14:08 +0200

    123

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Minigame from './Minigame';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <About />
+      <Minigame />
+    </div>
+  );
+}
+
+export default App;
