b81a2969d60662ddb9f8223ff1ce6c91985b8e32
snowhillv9 <81157167+snowhillv9@users.noreply.github.com>
2022-04-25 14:16:05 +0200

    sådan får man billede ind
    billede

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import mig from "./mig.jpg"
 
 const Home = () =>{
     return(
         <div>
             <h1>Home</h1>
+            <div className="logo">
+          <img src={mig} width="100" height="50" />
+        </div>
+           
         </div>
     )
 }
--- src/Minigames.js
+++ src/Minigames.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import mig from "./mig.jpg"
 
 const Minigames = () =>{
     return(
         <div>
             <h1>Minigames</h1>
+            <div className="logo">
+          <img src={mig} width="100" height="50" />
+        </div>
         </div>
     )
 }
