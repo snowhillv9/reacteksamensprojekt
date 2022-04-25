7e35cced837d0bfd8ebdc20bce04483d16d01844
snowhillv9 <81157167+snowhillv9@users.noreply.github.com>
2022-04-25 13:46:53 +0200

    kms
    kms

--- src/Application.js
+++ src/Application.js
@@ -1,28 +1,26 @@
 import About from "./about";
 import Home from "./Home";
-import React from 'react'
+import React, { Fragment } from 'react'
 import Minigames from "./Minigames";
-import Test from "./test";
 import "./App.css";
 import Nav from "./nav";
-import {BrowserRouter, BrowserRouter as Router, Link, Route} from'react-router-dom';
+import {BrowserRouter, BrowserRouter as Router,Route} from'react-router-dom';
 import { Routes } from 'react-router-dom';
 
 
 const Application = () =>{
     return(
         <>
-        <BrowserRouter>
-       <Routes>
+        <Router>
            <div className="App">
-               <Nav/>
+       <Routes>
+       <Route path="/" element={<Nav/>} />
            <Route path="/" element={<Home/>} />
            <Route path="/About" element={<About/>} />
            <Route path="/Minigames" element={<Minigames/>} />
-           <Route path="/Test" element={<Test/>} />
-           </div>
        </Routes>
-       </BrowserRouter>
+           </div>
+       </Router>
        </>
     );
 }
