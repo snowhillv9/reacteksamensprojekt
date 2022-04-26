13d7b65076be6b6ccb7bc7713984c555c9abee80
kre1e <olesendk@hotmail.com>
2022-04-26 11:42:10 +0200

    button

--- src/Minigames.js
+++ src/Minigames.js
@@ -6,6 +6,15 @@ const Minigames = () =>{
         <div>
             <Nav/>
             <h1>Minigames</h1>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
         </div>
     )
 }
