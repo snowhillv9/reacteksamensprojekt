0f6ca2329bf6fa03b799bc1d9d1cf5ca370812c8
kre1e <olesendk@hotmail.com>
2022-04-26 11:55:38 +0200

    123

--- src/Minigames.js
+++ src/Minigames.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Nav from "./nav";
+import "./Button.css"
 
 const Minigames = () =>{
     return(
         <div>
             <Nav/>
             <h1>Minigames</h1>
+            <div className='button-grid'>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            </div>
         </div>
     )
 }
