82e353797620d7959f0ff9aa85ba72ac34dcdfbc
snowhillv9 <81157167+snowhillv9@users.noreply.github.com>
2022-04-26 14:08:58 +0200

    skmub
    skub

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Home(){
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Profile
+            </h1>
         </div>
     )
 }
--- src/nav.js
+++ src/nav.js
@@ -11,7 +11,7 @@ function Nav(){
            <h3>Human Benchmark</h3>
                <ul className='nav-links'>
                 <li><Link to="/">Home Page</Link></li>
-                <li><Link to="Home">Home</Link></li>
+                <li><Link to="Home">Profile</Link></li>
                 <li><Link to="About">About</Link></li>
                 <li><Link to="Minigames">Minigames</Link></li>
                </ul>  
