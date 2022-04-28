93e895f08f58d95d273d6f61c2b7dd698d195ce9
kre1e <olesendk@hotmail.com>
2022-04-28 12:11:23 +0200

    123

---
+++ src/Application.js
@@ -0,0 +1,38 @@
+import About from "./about";
+import Home from "./Home";
+import Minigames from "./Minigames";
+import React, { Fragment } from 'react'
+import "./App.css";
+import Nav from "./nav";
+import SequenceMemory from "./sequenceMemory";
+import {BrowserRouter, BrowserRouter as Router,Route,Link} from'react-router-dom';
+import { Routes } from 'react-router-dom';
+import ErrorPage from "./ErrorPage";
+
+
+const Application = () =>{
+    return (
+        
+        <div className="App">
+          
+          <Router>
+          <div className="list">
+              <ul>
+                <Nav/>
+              </ul>
+            </div>
+            <Routes>
+              <Route path="/" element={<h1>Home Page</h1>} />
+              <Route path="/Home" element={<Home/>} />
+              <Route path="/About" element={<About/>} />
+              <Route path="/Minigames" element={<Minigames/>} />
+              <Route path="/SequenceMemoryGame" element={<SequenceMemory/>} />
+              <Route path="/*" element={<ErrorPage/>} />
+            </Routes>
+            
+          </Router>
+        </div>
+      );
+}
+
+export default Application;
---
+++ src/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Nav from './nav';
+
+
+export default function ErrorPage() {
+    return (
+        <div>
+            ERROR! PAGE WAS NOT FOUND!</div>
+    )
+}
