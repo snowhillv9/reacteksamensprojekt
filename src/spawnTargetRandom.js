a559df877aa9d4dba51b6e356a7b20b06b22aed4
snowhillv9 <81157167+snowhillv9@users.noreply.github.com>
2022-05-09 21:13:20 +0200

    Update spawnTargetRandom.js

--- src/spawnTargetRandom.js
+++ src/spawnTargetRandom.js
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef,useState, useEffect } from 'react';
 
 const Image = () => {
+  const { height, width } = useWindowDimensions();
   const testStyles = useRef({
-      position: "absolute",
+      position: "absolute", width: Math.random*(width)},
       top: Math.floor(Math.random() * 50),
       left: "50%",
       transform: "translate(-50%, -50%)",
@@ -10,7 +11,32 @@ const Image = () => {
 
   return <img src ="./target.png" style={testStyles.current} alt="" />;
 };
-     
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+ function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {     //Useeffect Bliver kaldt første gang vores komponent bliver vist - svarer til component.
+    //Did mount + componentdidupdate derudover "detekter" den hvornår en side bliver renderet eller re-renderet
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+
  export default Image;
 
 
