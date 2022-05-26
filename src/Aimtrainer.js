221d57279d4b8dc08e00b9b576054611978c811b
snowhillv9 <81157167+snowhillv9@users.noreply.github.com>
2022-05-26 14:15:46 +0200

    xd
    xd

--- src/Aimtrainer.js
+++ src/Aimtrainer.js
@@ -3,34 +3,23 @@ import { useState } from "react";
 import { click } from "@testing-library/user-event/dist/click";
 import AimTrainerLogic from "./Aimtrainerlogic";
 
-export default function Aimtrainer(){
-    const[clicked, setClicked] = useState(false);
+export default function Aimtrainer() {
+  const [clicked, setClicked] = useState(false);
 
-    const handleClick = () => {
-        setClicked(!clicked);
-    }
-    const isClicked = clicked;
-    
-    if(clicked){
-        return(
-        <>
-        <AimTrainerLogic/>
-        </>
-        )
-    }
-    return(
-        <div>
-            <h1>Aimtrainer</h1>
-            <button onClick={handleClick}>Start Game</button>
-        </div>
-    )
-
-    function StopGame(){
-
-        // let targetsHit = ButtonState.value
-        //Kalder på countdownTimerApp eller lignende og lytter efter hvad counteren er. Er counteren 0 stoppes spillet og der bliver printet, hvor mange targets spilleren har skudt. 
-    }
-
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+  if (clicked) {
+    return (
+      <>
+        <AimTrainerLogic />
+      </>
+    );
+  }
+  return (
+    <div>
+      <h1>Aimtrainer</h1>
+      <button onClick={handleClick}>Start Game</button>
+    </div>
+  );
 }
-
-
